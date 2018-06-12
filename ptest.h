1f1bebbe2759d4fa884ed3169d995c31394e243d
Unknown <peter.orzell@gmail.com>
2018-06-12 10:06:22 -0400

    Added support for Windows (MSVC)
    Added support for Assert messages as well.

--- ptest.h
+++ ptest.h
@@ -12,31 +12,55 @@
 #define TEST_FAIL -1
 #define TEST_SUCCESS 0
 
+#if defined(_MSC_VER)
+#pragma section(".CRT$XCU",read)
+#define INITIALIZER2_(f,p) \
+        static void f(void); \
+        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+        __pragma(comment(linker,"/include:" p #f "_")) \
+        static void f(void)
+#ifdef _WIN64
+#define INITIALIZER(f) INITIALIZER2_(f,"")
+#else
+#define INITIALIZER(f) INITIALIZER2_(f,"_")
+#endif
+#else
+#define INITIALIZER(f) \
+        static void f(void) __attribute__((constructor)); \
+        static void f(void)
+#endif
+
 #define P_TEST(TEST_NAME) \
     static int TEST_NAME(void);\
-    void __attribute__((constructor)) MAKE_UNIQUE_TESTNAME(TEST_NAME##_reg)(void){\
+    INITIALIZER(MAKE_UNIQUE_TESTNAME(TEST_NAME)){\
     P_AddTest(STRINGIFY(TEST_NAME), TEST_NAME);\
     }\
     static int TEST_NAME(void)
 
 #define P_TEST_F(TEST_NAME, CREATE_FUNCTION, TEARDOWN_FUNCTION, DATA) \
     static int TEST_NAME(void*);\
-    void __attribute__((constructor)) MAKE_UNIQUE_TESTNAME(TEST_NAME##_reg)(void){\
+    INITIALIZER(MAKE_UNIQUE_TESTNAME(TEST_NAME)){\
     P_AddTestWithFixture(STRINGIFY(TEST_NAME), TEST_NAME, CREATE_FUNCTION, TEARDOWN_FUNCTION, DATA);\
     }\
     static int TEST_NAME(void* data)
 
-static const char *ASSERT_FAIL_STR = "\nFailure in %s() %s:%u:  Assertion failed (\"%s\")\n";
+static const char *ASSERT_FAIL_STR = "\nFailure in %s() %s:%u:\n\tAssertion failed (\"%s\")\n";
+static const char *ASSERT_FAIL_MSG_STR = "Message: %s\n";
 
 #define P_ASSERT(expr) {if (!(expr)) { \
 printf(ASSERT_FAIL_STR, __FUNCTION__, __FILE__, __LINE__, STRINGIFY(expr)); \
 P_Fail(); } else { P_ClearFailMessage();}}
 
-typedef void* (*CREATE_FUNC)(void);
-typedef void (*TEARDOWN_FUNC)(void*);
+#define P_ASSERT_S(expr, msg) {if (!(expr)) { \
+printf(ASSERT_FAIL_STR, __FUNCTION__, __FILE__, __LINE__, STRINGIFY(expr)); \
+printf(ASSERT_FAIL_MSG_STR, msg); \
+P_Fail(); } else { P_ClearFailMessage();}}
 
-typedef int(*TEST_FUNC)();
-typedef int(*TEST_FIXTURE_FUNC)(void*);
+typedef void* (*CREATE_FUNC)(void);
+typedef void  (*TEARDOWN_FUNC)(void*);
+
+typedef int  (*TEST_FUNC)();
+typedef int  (*TEST_FIXTURE_FUNC)(void*);
 
 void P_AddTest(const char* test_name, TEST_FUNC testfunc);
 void P_AddTestWithFixture(const char* test_name, TEST_FIXTURE_FUNC test_func, CREATE_FUNC create_func, TEARDOWN_FUNC teardown_func, void* data);
